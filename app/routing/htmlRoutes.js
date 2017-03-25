aa80b7e52991471fbd68b0bbd615f26eba381ce4
Matt White <mattwhite1337@gmail.com>
2017-03-25 12:17:23 -0400

    login

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,10 @@
 var bcrypt = require("bcrypt");
 var db = require("../../models");
+var path = require("path");
 
 module.exports = function(app){
 	app.get('/', function(req, res) {
-		res.send('test');
+	  res.sendFile(path.join(__dirname,'/../public/', "index.html"));
 	});
 
 	app.get('/logout', function(req, res) {
